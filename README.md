7eb5fbb686c621a9786feb1ed69d97e402e03b3b
Connor Clark <cjamcl@gmail.com>
2023-10-08 20:08:24 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web.zquestclassic.com
+
+This repo contains the web deployment of ZQuest Classic, hosted at [web.zquestclassic.com](https://web.zquestclassic.com).
---
+++ scripts/update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p .tmp
+
+# cp _headers _site
+
+curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/releases/latest > .tmp/latest.json
+VERSION=$(jq -r '.tag_name' .tmp/latest.json)
+
+if [ ! -d .tmp/release-$VERSION ]; then
+    URL=$(jq -r '.assets[] | select(.name|endswith("web.zip")) | .browser_download_url' .tmp/latest.json)
+    wget $URL -O .tmp/web.zip
+    unzip .tmp/web.zip -d .tmp
+
+    rm .tmp/dist/index.html
+    sed -i 's@/zc/@/@g' .tmp/dist/create/index.html || (sed 's@/zc/@/@g' .tmp/dist/create/index.html > out.html && mv out.html .tmp/dist/create/index.html)
+    sed -i 's@/zc/@/@g' .tmp/dist/play/index.html || (sed 's@/zc/@/@g' .tmp/dist/play/index.html > out.html && mv out.html .tmp/dist/play/index.html)
+    npx --yes json -I -f .tmp/dist/manifest.json -e "this.scope='https://web.zquestclassic.com'"
+    mv .tmp/dist .tmp/release-$VERSION
+fi
+
+rm -rf .tmp/web.zip .tmp/latest.json
+
+rm -rf dist
+mkdir dist
+cp -r .tmp/release-$VERSION/* dist
