d46e1277c8f47af395240d6634395c279b5cee94
Connor Clark <cjamcl@gmail.com>
2024-01-23 21:41:40 -0800

    3.0.0-prerelease.32+2024-01-24

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,7 +7,7 @@ mkdir -p .tmp
 # TODO restore
 exit 0
 # curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/releases/latest > .tmp/latest.json
-curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/releases/tags/3.0.0-prerelease.24+2024-01-08 > .tmp/latest.json
+curl -L https://api.github.com/repos/ZQuestClassic/ZQuestClassic/releases/tags/3.0.0-prerelease.32+2024-01-24 > .tmp/latest.json
 VERSION=$(jq -r '.tag_name' .tmp/latest.json)
 
 if [ ! -d ".tmp/release-$VERSION" ]; then
